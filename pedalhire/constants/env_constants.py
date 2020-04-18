6efd0cfa971320459baa3e84b0210faa77f11fdd
Vivek Maskara <maskaravivek@gmail.com>
2020-04-18 15:38:03 -0700

    Fix env variables for mac

--- pedalhire/constants/env_constants.py
+++ pedalhire/constants/env_constants.py
@@ -9,10 +9,10 @@ if os.name == 'nt':
     ENVIRONMENT = os.getenv('ENVIRONMENT')
     VERSION = os.getenv('VERSION')
 elif os.name == 'posix':
-    SECRET_KEY = os.environ('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ('SQLALCHEMY_DATABASE_URI')
-    SUPPORT_EMAIL = os.environ('SUPPORT_EMAIL')
-    SEND_GRID_API_KEY = os.environ('SEND_GRID_API_KEY')
-    SENTRY_DSN_API = os.environ('SENTRY_DSN_API')
-    ENVIRONMENT = os.environ('ENVIRONMENT')
-    VERSION = os.environ('VERSION')
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SUPPORT_EMAIL = os.environ['SUPPORT_EMAIL']
+    SEND_GRID_API_KEY = os.environ['SEND_GRID_API_KEY']
+    SENTRY_DSN_API = os.environ['SENTRY_DSN_API']
+    ENVIRONMENT = os.environ['ENVIRONMENT']
+    VERSION = os.environ['VERSION']
