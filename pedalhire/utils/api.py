3d285b0201afad3701ebee03ee17c8c47eaa9f67
Vivek Maskara <maskaravivek@gmail.com>
2020-04-18 13:17:06 -0700

    Add decorator cache headers

--- pedalhire/utils/api.py
+++ pedalhire/utils/api.py
@@ -1,4 +1,17 @@
-from flask import make_response, jsonify
+from flask import make_response, jsonify, Response, json
+from functools import wraps
 
 def handle_response(response={}, status=200):
     return make_response(jsonify(response)), status
+
+def docache(minutes=5):
+    """ Flask decorator that allow to set Cache headers. """
+    def fwrap(f):
+        @wraps(f)
+        def wrapped_f(*args, **kwargs):
+            r = f(*args, **kwargs)
+            rsp = Response(json.dumps(r), content_type='application/json; charset=utf-8')
+            rsp.headers.add('Cache-Control', 'public,max-age=%d' % int(60 * minutes))
+            return rsp
+        return wrapped_f
+    return fwrap
