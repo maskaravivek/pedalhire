db6efd1df033c70d67e3066be4cd3d24b191b798
Raul9595 <rahulsvarm@gmail.com>
2020-04-24 16:37:18 -0700

    Revert "Restoring upload service"
    This reverts commit a35b9cc5ca7c44e63e42fb4faf80e8cdae92fcd0.

--- pedalhire/services/upload_service.py
+++ pedalhire/services/upload_service.py
@@ -2,14 +2,4 @@ from ..constants.env_constants import GOOGLE_CLOUD_BUCKET
 
 
 def upload_blob(source_file_name, destination_blob_name):
-    """Uploads a file to the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(GOOGLE_CLOUD_BUCKET)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_filename(source_file_name)
-    print(
-        "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
-        )
-    )
+    return True
