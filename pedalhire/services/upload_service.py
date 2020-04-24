4848774634ec422be9c1c5e1e774c1cdfd1aa5d9
Raul9595 <rahulsvarm@gmail.com>
2020-04-24 16:37:53 -0700

    Restoring upload blob file

--- pedalhire/services/upload_service.py
+++ pedalhire/services/upload_service.py
@@ -1,5 +1,16 @@
 from ..constants.env_constants import GOOGLE_CLOUD_BUCKET
+from google.cloud import storage
 
 
 def upload_blob(source_file_name, destination_blob_name):
-    return True
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(GOOGLE_CLOUD_BUCKET)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
